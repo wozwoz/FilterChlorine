e08725818f40825dd50f619c491e7a3a9c6136a4
Warren Turner <woz@pi.hole>
2026-01-18 19:43:31 +1100

    Initial commit

---
+++ lib/credentials/credentials.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+#define WIFI_SSID "BurradooAP"
+#define WIFI_PASS "2rachelle"
+
+#define MQTT_HOST "192.168.1.204"
+#define MQTT_PORT 1883
+const char *mqtt_broker = "192.168.1.204";
+const char *mqtt_user = "mqtt";
+const char *mqtt_password = "01July2025";
